0dca3410acbd56bd02297780d9f63d535fe2fbe5
Josh Montague <joshua.montague@gmail.com>
2022-11-27 20:16:17 -0700

    Query actual API, add chart and table

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,28 +7,34 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Page Config: Colorado COVID-19 Wastewater Data",
+    page_title="CO COVID-19 Wastewater",
     page_icon="🔬",
-    layout="centered",
+    layout="wide",
 )
 
-st.title("Title: Colorado COVID-19 Wastewater Data")
+st.title("Colorado COVID-19 Wastewater Data")
 
-st.write("All the same official data, without all the ArcGIS!")
+st.write(
+    """
+All the same official data, without all the ArcGIS!
 
-st.write("TODO: add some pointers for less obvious utilities around Denver")
+- TODO: add context for why this exists
+- TODO: add links to CDPHE site
+- TODO: add some names for less obvious utilities around Denver
+"""
+)
 
 
-# TODO: query API for utilities
-# utilities = requests.get('https://dummyjson.com/products/categories')
+# TODO: cache this with ttl
+# TODO: add header info to id this app in access logs
+utilities = requests.get("https://dummyjson.com/products/categories")
 
-# select params via dropdown
+# construct API query via dropdowns
 col1, col2 = st.columns(2)
 with col1:
     utility = st.selectbox(
         label="Choose a wastewater utility",
-        # TODO: load options via request
-        options=("Email", "Home phone", "Mobile phone"),
+        options=utilities,
         index=0,
     )
 with col2:
@@ -45,7 +51,25 @@ with col2:
     else:
         diff = timedelta(days=30 * 12 * 3)
     start = end - diff
-    start_param = start.isoformat()
-    end_param = end.isoformat()
 
-st.write(f"calculated daterange is {start=} - {end=} ")
+# TODO: set up proper DNS
+base_url = "http://146.190.50.82"
+api_params = (
+    f"/api/samples?utility={utility}&start={start.isoformat()}&end={end.isoformat()}"
+)
+
+# TODO: cache with ttl
+data = requests.get(base_url + api_params).json()
+
+this_utility = data["parameters"]["utility"]
+this_start = data["parameters"]["start"]
+this_end = data["parameters"]["end"]
+this_report = data["samples"]
+
+st.write(f"Displaying results for {this_utility} over range {this_start} - {this_end}")
+
+report_frame = pd.DataFrame(this_report, columns=["Date", "Samples"])
+
+st.bar_chart(report_frame)
+
+st.table(report_frame)
